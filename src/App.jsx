10b93a6b6f739d0f7e228ecaa62f9f7adddfa0cd
Abudi <abduselamt47@gmail.com>
2024-11-03 22:45:48 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [Advice,  setAdvice] = useState ("");
+
+  async function Getmessage () {
+    const res = await fetch ("https://api.adviceslip.com/advice")
+    const data = await res.json();
+    setAdvice(data.slip.advice)
+    setCount((c) => c+1  );
+  }
+
+  return (
+    <>
+      <h1>{Advice}</h1>
+
+      <button onClick={Getmessage}> Get Advice</button>
+      <Meassage count = {count} />
+
+      <footer>
+        developed by Abudi47
+      </footer>
+      
+    </>
+  )
+}
+function Meassage(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong>Piece of advice 
+    </p>
+  )
+}
+export default App
